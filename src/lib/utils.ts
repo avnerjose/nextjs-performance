e6e53c9900ca1a8ac41683dce35bc84dd43375ab
Avner José <avnerj.g.r@gmail.com>
2023-09-20 19:58:53 -0300

    Importing shadcn components

---
+++ src/lib/utils.ts
@@ -0,0 +1,36 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import qs from "query-string";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+interface UrlQueryParams {
+  params: string;
+  key?: string;
+  value?: string | null;
+  keysToRemove?: string[];
+}
+
+export function formUrlQuery({
+  params,
+  key,
+  value,
+  keysToRemove,
+}: UrlQueryParams) {
+  const currentUrl = qs.parse(params);
+
+  if (keysToRemove) {
+    keysToRemove.forEach((keyToRemove) => {
+      delete currentUrl[keyToRemove];
+    });
+  } else if (key && value) {
+    currentUrl[key] = value;
+  }
+
+  return qs.stringifyUrl(
+    { url: window.location.pathname, query: currentUrl },
+    { skipNull: true }
+  );
+}
